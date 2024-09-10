9da748cdb4abdf98886b65f0f016d6966272d346
rafiadadisini <lagirafi@gmail.com>
2024-09-11 01:05:55 +0700

    Update Tampilan Section Skill

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./public/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js",
+    './index.html',
+    './public/**/*.{html,js}',
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {
       colors: {
-        rafcolor: "#0d76ba",
+        rafcolor: '#0d76ba',
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require('flowbite/plugin')],
 };
