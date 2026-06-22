210b08b93202e18bcb9bd76c6d03ad5f2640a5da
rafi-coding-user <lagirafi@gmail.com>
2026-06-22 22:38:45 +0700

    feat: change theme toggle to single icon format and update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Menggunakan class-based dark mode untuk integrasi Tailwind
   content: [
     './index.html',
     './public/**/*.{html,js}',
